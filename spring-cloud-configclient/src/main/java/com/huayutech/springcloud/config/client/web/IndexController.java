bd65ddfefe120a8c560e44dfe739ba2aa19eed06
yoyoyosiyu <yoyoyosiyu@126.com>
2018-06-14 17:28:35 +0800

    frist commit

--- spring-cloud-configclient/src/main/java/com/huayutech/springcloud/config/client/web/IndexController.java
+++ spring-cloud-configclient/src/main/java/com/huayutech/springcloud/config/client/web/IndexController.java
@@ -1,18 +1,23 @@
 package com.huayutech.springcloud.config.client.web;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class IndexController {
 
     @Value("${spring.boot}")
     String greeting;
 
+    @Value("${application.company}")
+    String companyName;
+
     @GetMapping("/")
     public String doGet() {
-        return greeting;
+        return greeting +"," + companyName;
     }
 
 }
